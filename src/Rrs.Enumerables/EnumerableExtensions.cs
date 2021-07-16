986994158050902c56dfeaba3bed88cc401a3b2b
Robert Snipe <robertjsnipe@gmail.com>
2021-07-16 14:36:41 +0100

    added spliterate and tryselect

--- src/Rrs.Enumerables/EnumerableExtensions.cs
+++ src/Rrs.Enumerables/EnumerableExtensions.cs
@@ -6,6 +6,8 @@ using System.Linq;
 
 namespace Rrs.Enumerables
 {
+    public delegate bool TryFunc<in TSource, TResult>(TSource arg, out TResult result);
+
     public static class EnumerableExtensions
     {
         public static HashSet<T> ToHashSet<T>(this IEnumerable<T> enumerable)
@@ -72,6 +74,54 @@ namespace Rrs.Enumerables
         public static IEnumerable<OrderedDictionary> ToOrderedDictionary(this DataTable table)
         {
             return table.Select().ToOrderedDictionary();
+        }
+
+        public static IEnumerable<TOut> SelectTry<TIn, TOut>(this IEnumerable<TIn> enumerable, TryFunc<TIn, TOut> selector)
+        {
+            foreach (var v in enumerable)
+            {
+                if (selector(v, out var r))
+                    yield return r;
+            }
+        }
+
+        public static void Spliterate<T>(this IEnumerable<T> enumerable, Predicate<T> predicate, Action<T> included, Action<T> excluded)
+        {
+            foreach (var e in enumerable)
+            {
+                if (predicate(e))
+                    included(e);
+                else
+                    excluded(e);
+            }
+        }
+
+        public static (IEnumerable<TOutIncluded> included, IEnumerable<TOutExcluded> excluded) Spliterate<T, TOutIncluded, TOutExcluded>(this IEnumerable<T> enumerable, Predicate<T> predicate, Func<T, TOutIncluded> includeSelector, Func<T, TOutExcluded> excludeSelector)
+        {
+            var included = new List<TOutIncluded>();
+            var excluded = new List<TOutExcluded>();
+            foreach (var e in enumerable)
+            {
+                if (predicate(e))
+                    included.Add(includeSelector(e));
+                else
+                    excluded.Add(excludeSelector(e));
+            }
+            return (included, excluded);
+        }
+
+        public static (IEnumerable<TOut> included, IEnumerable<TOut> excluded) Spliterate<T, TOut>(this IEnumerable<T> enumerable, Predicate<T> predicate, Func<T, TOut> selector)
+        {
+            var included = new List<TOut>();
+            var excluded = new List<TOut>();
+            foreach (var e in enumerable)
+            {
+                if (predicate(e))
+                    included.Add(selector(e));
+                else
+                    excluded.Add(selector(e));
+            }
+            return (included, excluded);
         }
     }
 }
